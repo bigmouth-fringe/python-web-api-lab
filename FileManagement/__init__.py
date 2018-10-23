281f0e6a2531b8a322b05730d08e552d1770dfc3
BigmouthFringe <jesterdead97@gmail.com>
2018-10-24 01:14:07 +0700

    Add root API method. Returns ugly JSON atm

--- FileManagement/__init__.py
+++ FileManagement/__init__.py
@@ -1,6 +1,8 @@
 import os
 
 from flask import Flask
+from flask import Response
+from flask import jsonify
 import markdown
 
 # Instance of FLask
@@ -11,4 +13,13 @@ def index():
     with open(os.path.dirname(app.root_path) + '/README.md', 'r') as markdown_file:
         content = markdown_file.read()
         return markdown.markdown(content)
+
+@app.route("/root", methods=['GET'])
+def root():
+    with open(f'{__file__}', 'r') as file:
+        response = {
+            "title": os.path.basename(file.name),
+            "content": file.read()
+        }
+        return jsonify(response)
         
